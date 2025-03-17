030f5b1aa40f1524348e1fbb61be7871bbc42fbe
OgunLouis <ogunnowolouis@gmail.com>
2025-03-17 11:11:40 +0100

    louis update

--- NkechiDiner/settings.py
+++ NkechiDiner/settings.py
@@ -38,7 +38,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "nkechidiner.onrender.com").split()
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "nkechidiner.onrender.com").split(",")
+
 
 
 
